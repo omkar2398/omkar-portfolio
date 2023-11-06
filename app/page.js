4b07d10f7b593d62015e38b463435bdd924823bd
unknown <omkargovalkar23@gmail.com>
2023-11-06 22:34:54 +0530

    Minimal Update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import LiveTicker from "./components/LiveTicker";
 
 export default function Home() {
   return (
     <div>
+      <LiveTicker />
       <Navbar />
       <Hero />
     </div>
