a655c2554b05652c2f282e5f79e8519a917f01e9
unknown <omkargovalkar23@gmail.com>
2023-11-06 22:29:12 +0530

    Installed spline library

---
+++ app/components/Hero.js
@@ -0,0 +1,39 @@
+"use client"; // This is a client component 👈🏽
+
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto my-8">
+      <div className="relative h-screen">
+        {/* <Spline scene="https://prod.spline.design/OKnzKfv36STXyzgb/scene.splinecode" /> */}
+        <div className="absolute top-1/3 m-6">
+          <h1 className="text-3xl md:text-7xl font-black text-black">OMKAR</h1>
+          <h1 className="text-3xl md:text-7xl font-black text-black">
+            GOVALKAR
+          </h1>
+          <p className="text-xl md:text-3xl opacity-50 text-black">
+            A frontend developer based in Mumbai, India.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
+
+// Extra models
+{
+  /* <Spline scene="https://prod.spline.design/f1Sxh74illDCCthN/scene.splinecode" /> */
+}
+{
+  /* <Spline scene="https://prod.spline.design/7EeBex4DlcE3SwE3/scene.splinecode" /> */
+}
+{
+  /* <Spline scene="https://prod.spline.design/bfrqvyqsAeyrnTLV/scene.splinecode" /> */
+}
+{
+  /* <Spline scene="https://prod.spline.design/bfrqvyqsAeyrnTLV/scene.splinecode" /> */
+}
